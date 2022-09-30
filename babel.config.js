9844f8807da157c5e41881ab9e0710ae4894f018
yuda1124 <yuda1124@naver.com>
2022-10-01 00:06:07 +0900

    chore: setting for dotenv

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,12 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'module:react-native-dotenv',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+        },
+      ],
       [
         'module-resolver',
         {
---
+++ src/types/@types/env.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-native-dotenv' {
+  export const GOOGLE_EXPO_CLIENT_ID: string;
+  export const GOOGLE_IOS_CLIENT_ID: string;
+  export const GOOGLE_ANDROID_CLIENT_ID: string;
+}
