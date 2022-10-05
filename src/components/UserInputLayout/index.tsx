a23d334a80d66ec5c841057521ea3e19332d7d95
yuda1124 <yuda1124@naver.com>
2022-10-06 01:04:49 +0900

    feat: implement layout componet for user input

---
+++ src/components/UserInputLayout/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from '..';
+import { COLOR } from '@/constants/styles/colors';
+import { TEXT_STYLE } from '@/constants/styles/textStyles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  content: {
+    flex: 1,
+    position: 'relative',
+  },
+  text: {
+    color: COLOR.mono.black,
+    marginBottom: 38,
+    marginTop: 24,
+  },
+  wrapButton: {
+    alignItems: 'center',
+    height: 48,
+    marginBottom: 34,
+  },
+});
+
+interface UserInputLayoutProps {
+  children: React.ReactNode;
+  infoMessage: string;
+  title: string;
+  gap?: number;
+  button?: React.ReactNode;
+}
+
+const UserInputLayout = ({
+  children,
+  infoMessage,
+  title,
+  gap = 0,
+  button,
+}: UserInputLayoutProps) => {
+  const renderChildren = () =>
+    React.Children.map(children, (child) => (
+      <View style={{ marginBottom: gap }}>{child}</View>
+    ));
+  return (
+    <View style={styles.container}>
+      <Header headerTitle={title} />
+      <Text style={[styles.text, TEXT_STYLE.title20M]}>{infoMessage}</Text>
+      <View style={styles.content}>{renderChildren()}</View>
+      {button && <View style={styles.wrapButton}>{button}</View>}
+    </View>
+  );
+};
+
+export { UserInputLayout };
