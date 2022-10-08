458ddc6ba7791b22685541f65477fa509c869f9c
yuda1124 <yuda1124@naver.com>
2022-10-09 01:17:00 +0900

    chore: add environment variables for firebase

--- src/types/@types/env.d.ts
+++ src/types/@types/env.d.ts
@@ -2,4 +2,11 @@ declare module 'react-native-dotenv' {
   export const GOOGLE_EXPO_CLIENT_ID: string;
   export const GOOGLE_IOS_CLIENT_ID: string;
   export const GOOGLE_ANDROID_CLIENT_ID: string;
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
 }
