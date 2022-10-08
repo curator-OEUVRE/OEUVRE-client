7e5f48c9283d4150b57928fbebba7516a7b1799d
Go Gwangseo <rufflain918@gmail.com>
2022-10-09 01:57:37 +0900

    feat: add type definition for env

--- src/types/@types/env.d.ts
+++ src/types/@types/env.d.ts
@@ -2,4 +2,5 @@ declare module 'react-native-dotenv' {
   export const GOOGLE_EXPO_CLIENT_ID: string;
   export const GOOGLE_IOS_CLIENT_ID: string;
   export const GOOGLE_ANDROID_CLIENT_ID: string;
+  export const KAKAO_APP_KEY: string;
 }
