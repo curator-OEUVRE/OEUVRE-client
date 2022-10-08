6b98adeb4fa924917dd2206564f49ea2b8455892
yuda1124 <yuda1124@naver.com>
2022-10-09 01:20:53 +0900

    feat: add logics for image upload

--- src/feature/UserProfileForm/index.tsx
+++ src/feature/UserProfileForm/index.tsx
@@ -1,14 +1,17 @@
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
-import { Text, StyleSheet, Pressable, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import AddCircleIcon from '@/assets/icons/AddCircle';
 import AddProfileIcon from '@/assets/icons/AddProfile';
 import {
   Button,
   FormInput,
   FormInputStatus,
+  Profile,
   UserInputLayout,
 } from '@/components';
 import { COLOR, TEXT_STYLE } from '@/constants/styles';
+import useUploadImage from '@/hooks/useUploadImage';
 import {
   validateExhibitionName,
   validateIntroduceMessage,
@@ -16,10 +19,20 @@ import {
 import { useSignUpStore } from '@/states/signUpStore';
 
 const styles = StyleSheet.create({
+  addIcon: {
+    bottom: 5,
+    position: 'absolute',
+    right: -15,
+  },
   buttonText: {
     color: COLOR.mono.white,
   },
   label: { color: COLOR.mono.black, marginBottom: 8 },
+  pressable: {
+    height: 125,
+    position: 'relative',
+    width: 125,
+  },
   wrapProfile: {
     alignItems: 'center',
   },
@@ -33,10 +46,10 @@ const UserProfileForm = () => {
     setProfileImageUrl,
     introduceMessage,
     setIntroduceMessage,
+    userId,
   } = useSignUpStore();
-
   const [image, setImage] = useState<string | undefined>(undefined);
-
+  const { uploading, uploadImage } = useUploadImage({ image });
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -44,14 +57,24 @@ const UserProfileForm = () => {
       aspect: [4, 3],
       quality: 1,
     });
-    console.log(result);
     if (!result.cancelled) {
       setImage(result.uri);
     }
   };
 
+  const onCompleteUpload = (url: string) => {
+    setProfileImageUrl({
+      ...profileImageUrl,
+      value: url,
+    });
+  };
+
   const button = (
-    <Button>
+    <Button
+      onPress={async () => {
+        await uploadImage('Profile', userId.value, onCompleteUpload);
+      }}
+    >
       <Text style={styles.buttonText}>설정 완료하기</Text>
     </Button>
   );
@@ -64,9 +87,12 @@ const UserProfileForm = () => {
     >
       <View>
         <Text style={[TEXT_STYLE.body14B, styles.label]}>프로필 사진</Text>
-        <Pressable style={styles.wrapProfile} onPress={pickImage}>
-          <AddProfileIcon />
-        </Pressable>
+        <View style={styles.wrapProfile}>
+          <Pressable style={styles.pressable} onPress={pickImage}>
+            {image ? <Profile imageUrl={image} /> : <AddProfileIcon />}
+            <AddCircleIcon style={styles.addIcon} />
+          </Pressable>
+        </View>
       </View>
       <FormInput
         label="전시회 이름"
