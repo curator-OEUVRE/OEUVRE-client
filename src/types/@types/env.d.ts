c47c0d1a0863941142a1e5e060b835fedee2220e
yuda1124 <yuda1124@naver.com>
2022-10-09 17:29:36 +0900

    chore: replace env variables for firebase google oauth

--- src/types/@types/env.d.ts
+++ src/types/@types/env.d.ts
@@ -1,7 +1,5 @@
 declare module 'react-native-dotenv' {
-  export const GOOGLE_EXPO_CLIENT_ID: string;
-  export const GOOGLE_IOS_CLIENT_ID: string;
-  export const GOOGLE_ANDROID_CLIENT_ID: string;
+  export const GOOGLE_CLIENT_ID: string;
   export const FIREBASE_API_KEY: string;
   export const FIREBASE_AUTH_DOMAIN: string;
   export const FIREBASE_PROJECT_ID: string;
