33c0f667504dcc223ba81b9e769d7c4c81786d40
Go Gwangseo <rufflain918@gmail.com>
2022-10-17 17:55:58 +0900

    feat: implement drag and drop feature

--- src/feature/FloorPictureList/FloorPictureList.stories.tsx
+++ src/feature/FloorPictureList/FloorPictureList.stories.tsx
@@ -1,7 +1,6 @@
 import { action } from '@storybook/addon-actions';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
-import { View } from 'react-native';
 import FloorPictureList from '.';
 import type { PictureInfo } from '@/states/createFloorStore';
 
@@ -10,29 +9,37 @@ const PICTURES: PictureInfo[] = [
     imageUri:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png',
     description: 'test',
-    height: 30,
+    width: 640 / 3 / 375,
+    height: 480 / 3 / 375,
     location: 0,
+    hashtags: [],
   },
   {
     imageUri:
       'https://cdn.searchenginejournal.com/wp-content/uploads/2022/06/image-search-1600-x-840-px-62c6dc4ff1eee-sej-1520x800.png',
     description: 'test',
-    height: 30,
+    width: 1600 / 5 / 375,
+    height: 840 / 5 / 375,
     location: 20 / 375,
+    hashtags: [],
   },
   {
     imageUri:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNoYW5nZXxlbnwwfHwwfHw%3D&w=1000&q=80',
     description: 'test',
-    height: 50,
+    width: 1000 / 4 / 375,
+    height: 667 / 4 / 375,
     location: 30 / 375,
+    hashtags: [],
   },
   {
     imageUri:
       'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
     description: 'test',
-    height: 30,
+    width: 771 / 3 / 375,
+    height: 480 / 3 / 375,
     location: -50 / 375,
+    hashtags: [],
   },
 ];
 
--- src/feature/FloorPictureList/index.tsx
+++ src/feature/FloorPictureList/index.tsx
@@ -1,41 +1,103 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import { StyleSheet, Pressable, Image, Dimensions } from 'react-native';
 import DraggableFlatList, {
   ScaleDecorator,
   RenderItemParams,
 } from 'react-native-draggable-flatlist';
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  FadeIn,
+  withSpring,
+  FadeOut,
+  useDerivedValue,
+} from 'react-native-reanimated';
+import AddCircleIcon from '@/assets/icons/AddCircle';
+import { COLOR } from '@/constants/styles';
 import type { PictureInfo } from '@/states/createFloorStore';
 
 const { width } = Dimensions.get('screen');
 
+const LINE_BUTTON_SIZE = 40;
+
 const styles = StyleSheet.create({
-  // contentContainer: {
-  //   alignItems: 'center',
-  // },
   flatList: {
     height: '100%',
   },
   item: {
     alignSelf: 'center',
   },
+  line: {
+    alignItems: 'center',
+    backgroundColor: COLOR.mono.gray3,
+    height: width * 0.5,
+    justifyContent: 'center',
+  },
+  lineContainer: {
+    alignSelf: 'center',
+    height: width * 0.5,
+  },
 });
 
-interface FloorPictureProps extends RenderItemParams<PictureInfo> {}
+interface FloorPictureProps extends RenderItemParams<PictureInfo> {
+  isDragging: SharedValue<boolean>;
+  editable?: boolean;
+}
 
-const FloorPicture = ({ item, drag }: FloorPictureProps) => {
-  const [imageWidth, setImageWidth] = useState(100);
-  const imageHeight = useMemo(() => width * (item.height / 100), [item.height]);
+const FloorPicture = ({
+  item,
+  drag,
+  isDragging,
+  getIndex,
+  editable = false,
+}: FloorPictureProps) => {
+  const [isLineActive, setIsLineActive] = useState(false);
+  const imageWidth = useMemo(() => width * item.width, [item.width]);
+  const imageHeight = useMemo(() => width * item.height, [item.height]);
 
-  useEffect(() => {
-    Image.getSize(item.imageUri, (w, h) => {
-      setImageWidth((w * imageHeight) / h);
-    });
-  }, [imageHeight, item.imageUri]);
+  const index = getIndex();
+
+  const animStyle = useAnimatedStyle(() => ({
+    width: withSpring(isDragging.value ? 0 : 1),
+    opacity: withSpring(isDragging.value ? 0 : 1),
+  }));
+
+  const containerAnimStyle = useAnimatedStyle(() => ({
+    paddingHorizontal: withSpring(
+      isLineActive && !isDragging.value ? 28 + LINE_BUTTON_SIZE / 2 : 24,
+    ),
+  }));
+
+  const line = (
+    <Pressable
+      onPress={() => {
+        setIsLineActive((prev) => !prev);
+      }}
+      style={styles.item}
+    >
+      <Animated.View style={[styles.lineContainer, containerAnimStyle]}>
+        <Animated.View style={[styles.line, animStyle]}>
+          {isLineActive && (
+            <Pressable>
+              <Animated.View entering={FadeIn} exiting={FadeOut}>
+                <AddCircleIcon
+                  width={LINE_BUTTON_SIZE}
+                  height={LINE_BUTTON_SIZE}
+                />
+              </Animated.View>
+            </Pressable>
+          )}
+        </Animated.View>
+      </Animated.View>
+    </Pressable>
+  );
 
   return (
     <ScaleDecorator>
+      {editable && index !== 0 && line}
       <Pressable
         style={[
+          /* eslint-disable-next-line react-native/no-inline-styles */
           {
             width: imageWidth,
             height: imageHeight,
@@ -44,10 +106,11 @@ const FloorPicture = ({ item, drag }: FloorPictureProps) => {
                 translateY: item.location * width,
               },
             ],
+            marginHorizontal: editable ? undefined : 16,
           },
           styles.item,
         ]}
-        onLongPress={drag}
+        onLongPress={editable ? drag : undefined}
       >
         <Image
           source={{ uri: item.imageUri }}
@@ -69,10 +132,20 @@ interface Props {
 
 const keyExtractor = (item: PictureInfo) => item.imageUri;
 
-const FloorPictureList = ({ pictures }: Props) => {
+const FloorPictureList = ({ pictures, editable }: Props) => {
+  const x = useRef<SharedValue<number>>();
+  const y = useRef<SharedValue<number>>();
+  const activeIndexAnim = useRef<SharedValue<number>>();
+
+  const isDragging = useDerivedValue(
+    () => activeIndexAnim.current?.value !== -1,
+  );
+
   const renderItem = useCallback(
-    (props: RenderItemParams<PictureInfo>) => <FloorPicture {...props} />,
-    [],
+    (props: RenderItemParams<PictureInfo>) => (
+      <FloorPicture {...props} isDragging={isDragging} editable={editable} />
+    ),
+    [isDragging, editable],
   );
 
   return (
@@ -81,8 +154,14 @@ const FloorPictureList = ({ pictures }: Props) => {
       renderItem={renderItem}
       keyExtractor={keyExtractor}
       horizontal
-      // contentContainerStyle={styles.contentContainer}
+      onAnimValInit={(animVals) => {
+        x.current = animVals.x;
+        y.current = animVals.y;
+        activeIndexAnim.current = animVals.activeIndexAnim;
+      }}
       style={styles.flatList}
+      /* eslint-disable-next-line react-native/no-inline-styles */
+      contentContainerStyle={{ paddingHorizontal: editable ? 60 : 48 }}
     />
   );
 };
