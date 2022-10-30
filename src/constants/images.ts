b296dba0e9e54b0f47eccd5bf1b5712ab35e492f
yuda1124 <yuda1124@naver.com>
2022-10-30 17:27:08 +0900

    chore: add check images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -4,4 +4,5 @@ export const IMAGE = {
   floorStyleShadow: require('@/assets/images/floorStyleShadow.png'),
   heart: require('@/assets/images/heart.png'),
   bookmark: require('@/assets/images/bookmark.png'),
+  check: require('@/assets/images/check.png'),
 } as const;
