512bbcaaf974035d7e9d6a13204a8b26dc846bac
yuda1124 <yuda1124@naver.com>
2022-10-30 17:27:36 +0900

    feat: implements success modal

--- src/screens/CreateFloorScreen/CreateFloorScreen.tsx
+++ src/screens/CreateFloorScreen/CreateFloorScreen.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable react-native/no-raw-text */
 import {
   CompositeNavigationProp,
   useNavigation,
 } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { BlurView } from 'expo-blur';
 import { lockAsync, OrientationLock } from 'expo-screen-orientation';
-import { useCallback, useLayoutEffect } from 'react';
-import { Pressable, StyleSheet, View } from 'react-native';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { Pressable, StyleSheet, View, Modal, Text, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CheckIcon from '@/assets/icons/Check';
 import { Header } from '@/components/Header';
+import { IMAGE } from '@/constants/images';
 import { Screen } from '@/constants/screens';
-import { COLOR } from '@/constants/styles';
+import { COLOR, TEXT_STYLE } from '@/constants/styles';
 import FloorPictureList from '@/feature/FloorPictureList';
 import { RootStackParamsList } from '@/feature/Routes';
 import { FloorStackParamsList } from '@/feature/Routes/FloorStack';
@@ -18,12 +21,28 @@ import useUploadImage from '@/hooks/useUploadImage';
 import { useCreateFloorStore } from '@/states/createFloorStore';
 
 const styles = StyleSheet.create({
+  check: {
+    height: 100,
+    width: 100,
+  },
   container: {
     backgroundColor: COLOR.mono.white,
     flex: 1,
   },
+  pressable: {
+    flex: 1,
+  },
+  text: {
+    color: COLOR.mono.black,
+    textAlign: 'center',
+  },
   wrapList: {
     flex: 1,
+  },
+  wrapModal: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
   },
 });
 
@@ -32,6 +51,26 @@ export type CreateFloorScreenNP = CompositeNavigationProp<
   StackNavigationProp<FloorStackParamsList, Screen.CreateFloorScreen>,
   StackNavigationProp<RootStackParamsList>
 >;
+
+interface SuccessModalProps {
+  onPress: () => void;
+}
+
+const SuccessModal = ({ onPress }: SuccessModalProps) => (
+  <Modal transparent animationType="fade">
+    <Pressable onPress={() => onPress()} style={styles.pressable}>
+      <BlurView intensity={100} tint="light" style={styles.wrapModal}>
+        <Image source={IMAGE.check} style={styles.check} />
+        <Text style={[styles.text, TEXT_STYLE.title20M]}>
+          축하드려요, {'\n'}
+          <Text style={TEXT_STYLE.title20B}>새로운 플로우가</Text>
+          오픈했어요!
+        </Text>
+      </BlurView>
+    </Pressable>
+  </Modal>
+);
+
 const CreateFloorScreen = () => {
   useLayoutEffect(() => {
     lockAsync(OrientationLock.LANDSCAPE);
@@ -39,6 +78,9 @@ const CreateFloorScreen = () => {
       lockAsync(OrientationLock.DEFAULT);
     };
   }, []);
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
   const navigation = useNavigation<CreateFloorScreenNP>();
   const { uploadImages } = useUploadImage();
   const { pictures, setPictures, color, name, createFloor } =
@@ -54,8 +96,8 @@ const CreateFloorScreen = () => {
     }));
     setPictures(newPictures);
     const result = await createFloor();
-    console.log(result.result);
-  }, [name.value, pictures, uploadImages, setPictures, createFloor]);
+    setModalVisible(true);
+  }, [createFloor, name.value, pictures, setPictures, uploadImages]);
 
   const ConfirmButton = useCallback(
     () => (
@@ -76,6 +118,7 @@ const CreateFloorScreen = () => {
           setPictures={setPictures}
         />
       </View>
+      {modalVisible && <SuccessModal onPress={() => console.log('press!!')} />}
     </SafeAreaView>
   );
 };
