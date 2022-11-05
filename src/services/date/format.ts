7a8c1a81b8d946fbb1c5cc6d526d0b7d038c4bb6
yuda1124 <yuda1124@naver.com>
2022-11-05 20:10:49 +0900

    feat: implement formatCreatedAt function

--- src/services/date/format.ts
+++ src/services/date/format.ts
@@ -1,7 +1,29 @@
-export function formatDate(date: Date) {
+const MIN = 60 * 1000;
+const HOUR = MIN * 60;
+const DAY = HOUR * 12;
+
+export const formatDate = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  return `${year}-${month < 10 ? `0${month}` : month}-${day}`;
-}
+  return `${year}-${month < 10 ? `0${month}` : month}-${
+    day < 10 ? `0${day}` : day
+  }`;
+};
+
+export const formatCreatedAt = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  if (diff >= DAY) {
+    return formatDate(date);
+  }
+  if (diff >= HOUR) {
+    return `${Math.floor(diff / HOUR)}시간`;
+  }
+  if (diff >= MIN) {
+    return `${Math.floor(diff / MIN)}분`;
+  }
+  return '방금';
+};
