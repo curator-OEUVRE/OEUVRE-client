2b1787029b8aab6799c7c8fdaf779a052f803a0f
yuda1124 <yuda1124@naver.com>
2022-11-05 20:03:58 +0900

    feat: define interface and types

---
+++ src/types/guestbook.ts
@@ -0,0 +1,30 @@
+import { ResponseDto } from './common';
+
+export interface GuestBookInfo {
+  comment: string;
+  commentNo: number;
+  createdAt: string;
+  isMine: boolean;
+  profileImageUrl: string;
+  userId: string;
+  userNo: number;
+}
+export interface CreateCommentRequestDto {
+  comment: string;
+}
+
+export interface OtherFloor {
+  floorNo: number;
+  queue: number;
+  name: string;
+}
+
+export interface GetCommentsResponse {
+  contents: GuestBookInfo[];
+  isLastPage: boolean;
+}
+
+export type CreateCommentResponseDto = ResponseDto<GuestBookInfo>;
+export type DeleteCommentResponseDto = ResponseDto<string>;
+export type GetCommentsResponseDto = ResponseDto<GetCommentsResponse>;
+export type GetOtherFloorsResponseDto = ResponseDto<OtherFloor[]>;
