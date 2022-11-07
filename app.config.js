433e62ab07c46d4060e7e3d04de71ec0e3478875
Go Gwangseo <rufflain918@gmail.com>
2022-11-08 02:12:24 +0900

    chore: add update setting

--- app.config.js
+++ app.config.js
@@ -19,6 +19,7 @@ module.exports = {
     },
     updates: {
       fallbackToCacheTimeout: 0,
+      url: 'https://u.expo.dev/bc755e58-fef2-4c34-8869-6f92386964d6',
     },
     assetBundlePatterns: ['**/*'],
     plugins: [
