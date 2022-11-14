c406b5f6f09594aa58cc50be46df6a6a0ccbd490
yuda1124 <yuda1124@naver.com>
2022-11-14 23:44:37 +0900

    feat: define notification type

---
+++ src/types/notification.ts
@@ -0,0 +1,22 @@
+import { ResponseDto } from './common';
+
+export interface Notification {
+  comment: string | null;
+  commentNo: number | null;
+  createdAt: string;
+  floorNo: number | null;
+  id: string | null;
+  isFollowing: boolean | null;
+  isRead: boolean;
+  pictureNo: number | null;
+  profileImageUrl: string;
+  sendUserNo: number;
+  type: 'FOLLOWING' | 'LIKES' | 'COMMENT';
+}
+
+export interface GetNotificationResponse {
+  contents: Notification[];
+  isLastPage: false;
+}
+
+export type GetNotificationResponseDto = ResponseDto<GetNotificationResponse>;
