663aa33cbf388620500c077b35419300ab25a8f4
yuda1124 <yuda1124@naver.com>
2022-11-14 23:45:38 +0900

    feat: implement follow button

---
+++ src/feature/FollowButton/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { COLOR, TEXT_STYLE } from '@/constants/styles';
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'flex-start',
+    backgroundColor: COLOR.mono.gray7,
+    borderRadius: 5,
+    height: 18,
+    paddingHorizontal: 8,
+  },
+  followingButton: {
+    backgroundColor: COLOR.mono.white,
+    borderColor: COLOR.mono.gray4,
+    borderWidth: 1,
+  },
+});
+
+interface FollowButtonProps {
+  isFollowing: boolean;
+  onPress?: () => void;
+}
+
+const FollowButton = ({ isFollowing, onPress }: FollowButtonProps) => {
+  const buttonStyle = isFollowing
+    ? {
+        ...styles.button,
+        ...styles.followingButton,
+      }
+    : styles.button;
+
+  return (
+    <Pressable
+      style={buttonStyle}
+      onPress={() => {
+        onPress?.();
+      }}
+    >
+      <Text
+        style={[
+          TEXT_STYLE.body12M,
+          { color: isFollowing ? COLOR.mono.gray4 : COLOR.mono.white },
+        ]}
+      >
+        {isFollowing ? '팔로잉' : '맞팔로우'}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default FollowButton;
