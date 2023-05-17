b1493ecdc2f9cd702e4a5546ee9433ad92f84049
yuda1124 <yuda1124@naver.com>
2023-05-17 23:36:53 +0900

    feat: implement rotate button component

---
+++ src/feature/RotateButton/index.tsx
@@ -0,0 +1,36 @@
+import {
+  getOrientationAsync,
+  lockAsync,
+  Orientation,
+  OrientationLock,
+} from 'expo-screen-orientation';
+import { useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import RotateIcon from '@/assets/icons/Rotate';
+import { IconButton } from '@/components/Button';
+
+const styles = StyleSheet.create({
+  container: {
+    bottom: 55,
+    position: 'absolute',
+    right: 20,
+  },
+});
+
+const RotateButton = () => {
+  const onPress = useCallback(async () => {
+    const orientation = await getOrientationAsync();
+    if (orientation < Orientation.LANDSCAPE_LEFT) {
+      await lockAsync(OrientationLock.LANDSCAPE_RIGHT);
+      return;
+    }
+    await lockAsync(OrientationLock.PORTRAIT_UP);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <IconButton icon={<RotateIcon />} onPress={onPress} />
+    </View>
+  );
+};
+
+export default RotateButton;
