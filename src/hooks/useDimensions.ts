4f3a4369a134d5cde32f9644509c08f133ab46b9
Gwangseo Go <rufflain918@gmail.com>
2023-05-24 02:20:46 +0900

    fix: use real height on picture

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -1,11 +1,7 @@
 import { Orientation } from 'expo-screen-orientation';
 import { Dimensions } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useDeviceStore } from '@/states/deviceStore';
-
-interface Dimension {
-  width: number;
-  height: number;
-}
 
 const WIDTH = Dimensions.get('screen').width;
 const HEIGHT = Dimensions.get('screen').height;
@@ -14,8 +10,9 @@ const HEIGHT = Dimensions.get('screen').height;
  * width, height은 항상 보이는 그대로의 가로/세로를 지칭.
  * 예를 들어 디바이스가 세로에서 가로로 바뀌면, 세로값이 가로값으로 바뀜
  */
-export const useDimensions = (): Dimension => {
+export const useDimensions = () => {
   const { orientation } = useDeviceStore();
+  const { top, bottom, left, right } = useSafeAreaInsets();
 
   const isLandscape =
     orientation === Orientation.LANDSCAPE_LEFT ||
@@ -24,9 +21,14 @@ export const useDimensions = (): Dimension => {
   const width = isLandscape ? HEIGHT : WIDTH;
   const height = isLandscape ? WIDTH : HEIGHT;
 
+  const widthOfSafeArea = width - left - right;
+  const heightOfSafeArea = height - top - bottom;
+
   return {
     width,
     height,
+    widthOfSafeArea,
+    heightOfSafeArea,
   };
 };
 
