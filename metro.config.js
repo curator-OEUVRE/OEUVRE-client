2b08ce8b7a92edb30aaf5d98c88e0257b9f401f5
Gwangseo Go <rufflain918@gmail.com>
2023-06-11 02:35:14 +0900

    build: resolve `DatePickerIOS` problem

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,15 @@ const defaultConfig = getDefaultConfig(__dirname);
 // firebase 설정을 위해 추가
 defaultConfig.resolver.assetExts.push('cjs');
 
+// https://github.com/facebook/react-native/issues/36794#issuecomment-1500880284
+defaultConfig.server = {
+  rewriteRequestUrl: (url) => {
+    if (!url.endsWith('.bundle')) {
+      return url;
+    }
+
+    return `${url}?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true`;
+  },
+};
+
 module.exports = defaultConfig;
