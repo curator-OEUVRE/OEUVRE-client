3547f258e44c1269270f7cd1479e4d322e53cb1a
yuda1124 <yuda1124@naver.com>
2023-06-19 22:28:59 +0900

    chore: set rotate button position

--- src/feature/RotateButton/index.tsx
+++ src/feature/RotateButton/index.tsx
@@ -5,19 +5,23 @@ import {
   OrientationLock,
 } from 'expo-screen-orientation';
 import { useCallback } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import RotateIcon from '@/assets/icons/Rotate';
 import { IconButton } from '@/components/Button';
 
 const styles = StyleSheet.create({
   container: {
+    marginRight: 20,
     position: 'absolute',
-    right: 20,
-    top: 8,
   },
 });
 
 const RotateButton = () => {
+  const { width, height } = useWindowDimensions();
+  const isLandscape = width > height;
+  const bottom = isLandscape ? 21 : 53;
+  const { right } = useSafeAreaInsets();
   const onPress = useCallback(async () => {
     const orientation = await getOrientationAsync();
     if (orientation < Orientation.LANDSCAPE_LEFT) {
@@ -27,7 +31,7 @@ const RotateButton = () => {
     await lockAsync(OrientationLock.PORTRAIT_UP);
   }, []);
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { bottom, right }]}>
       <IconButton icon={<RotateIcon />} onPress={onPress} />
     </View>
   );
